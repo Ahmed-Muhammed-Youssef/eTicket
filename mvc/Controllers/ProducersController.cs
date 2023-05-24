da11b124473b2f4dde8f8fd3bb4e6488c389fd0f
Ahmed Muhammed Youssef <ahmed.muhammed.youssef@gmail.com>
2023-05-24 15:31:14 +0300

    Add: producer controller

---
+++ mvc/Controllers/ProducersController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using mvc.Data;
+
+namespace mvc.Controllers
+{
+    public class ProducersController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ProducersController(AppDbContext appDbContext)
+        {
+            this._context = appDbContext;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var producers = await _context.Producer.ToListAsync();
+            return View(producers);
+        }
+    }
+}
