17d33ae903ee42f238c7371bbc70583b47fb8d6d
Ahmed Muhammed Youssef <ahmed.muhammed.youssef@gmail.com>
2023-05-24 15:37:18 +0300

    Add: cinemas controller

---
+++ mvc/Controllers/CinemasController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using mvc.Data;
+
+namespace mvc.Controllers
+{
+    public class CinemasController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public CinemasController(AppDbContext appDbContext)
+        {
+            this._context = appDbContext;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var cinemas = await _context.Cinema.ToListAsync();
+            return View(cinemas);
+        }
+    }
+}
