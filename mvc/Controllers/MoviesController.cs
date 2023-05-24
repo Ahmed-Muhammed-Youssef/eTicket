892e7901a85a1b48d840a8cf9c3d4f6e447da4b9
Ahmed Muhammed Youssef <ahmed.muhammed.youssef@gmail.com>
2023-05-24 15:45:48 +0300

    Add: movies controller

---
+++ mvc/Controllers/MoviesController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using mvc.Data;
+
+namespace mvc.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public MoviesController(AppDbContext appDbContext)
+        {
+            this._context = appDbContext;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var movies = await _context.Movie
+                .Include(m => m.Producer)
+                .Include(m => m.Cinema)
+                .ToListAsync();
+            return View(movies);
+        }
+    }
+}
