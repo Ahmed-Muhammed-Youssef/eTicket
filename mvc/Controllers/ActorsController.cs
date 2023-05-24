67e4d0124400e69a929459c1e32132cc2756b494
Ahmed Muhammed Youssef <ahmed.muhammed.youssef@gmail.com>
2023-05-24 15:22:51 +0300

    Add: actor controller

---
+++ mvc/Controllers/ActorsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using mvc.Data;
+
+namespace mvc.Controllers
+{
+    public class ActorsController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ActorsController(AppDbContext appDbContext)
+        {
+            this._context = appDbContext;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var actors = await _context.Actor.ToListAsync();
+            return View(actors);
+        }
+    }
+}
