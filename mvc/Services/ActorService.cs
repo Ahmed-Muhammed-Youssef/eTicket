ef3d7b19562c8e351a1a622564f5f753f93c5b85
Ahmed Youssef <ahmed.muhammed.youssef@gmail.com>
2023-06-21 04:14:34 +0300

    Update: ActorService to use the generic repository

--- mvc/Services/ActorService.cs
+++ mvc/Services/ActorService.cs
@@ -1,50 +1,15 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using mvc.Data;
+using mvc.Data.Base;
 using mvc.Interfaces;
 using mvc.Models;
 
 namespace mvc.Services
 {
-    public class ActorService : IActorService
+    public class ActorService : EntityBaseRepository<Actor>, IActorService
     {
-        private readonly AppDbContext _context;
-
-        public ActorService(AppDbContext context)
+        public ActorService(AppDbContext dbContext) : base(dbContext)
         {
-            this._context = context;
-        }
-        public async Task<Actor> AddAsync(Actor actor)
-        {
-            await _context.Actor.AddAsync(actor);
-            _context.SaveChanges();
-            return actor;
-        }
-
-        public async Task DeleteAsync(int id)
-        {
-            var actor = await _context.Actor.FindAsync(id);
-            if(actor == null)
-            {
-                throw new Exception($"Invalid Id: {id}");
-            }
-            _context.Actor.Remove(actor);
-            await _context.SaveChangesAsync();
-        }
-
-        public async Task<IEnumerable<Actor>> GetAllAsync()
-        {
-            return await _context.Actor.ToListAsync();
-        }
-
-        public async Task<Actor?> GetByIdAsync(int id)
-        {
-            return await _context.Actor.FindAsync(id);
-        }
-
-        public async Task<int> UpdateAsync(Actor actor)
-        {
-            _context.Actor.Entry(actor).State = EntityState.Modified;
-            return await _context.SaveChangesAsync();
         }
     }
 }
