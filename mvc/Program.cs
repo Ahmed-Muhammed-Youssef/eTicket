4b18c72855f807c0ab9c129440fc04ae8db2a8a2
Ahmed Youssef <ahmed.muhammed.youssef@gmail.com>
2023-06-25 06:26:54 +0300

    Add: cinema service

--- mvc/Program.cs
+++ mvc/Program.cs
@@ -15,6 +15,7 @@ builder.Services.AddDbContext<AppDbContext>(options => {
 // my services
 builder.Services.AddScoped<IActorService, ActorService>();
 builder.Services.AddScoped<IProducerService, ProducerService>();
+builder.Services.AddScoped<ICinemaService, CinemaService>();
 
 
 var app = builder.Build();
---
+++ mvc/Services/CinemaService.cs
@@ -0,0 +1,14 @@
+﻿using mvc.Data;
+using mvc.Data.Base;
+using mvc.Interfaces;
+using mvc.Models;
+
+namespace mvc.Services
+{
+    public class CinemaService : EntityBaseRepository<Cinema>, ICinemaService
+    {
+        public CinemaService(AppDbContext dbContext) : base(dbContext)
+        {
+        }
+    }
+}
