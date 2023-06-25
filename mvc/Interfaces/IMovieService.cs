2167da157b1dbc6f47561ec7516123e1d73584d4
Ahmed Youssef <ahmed.muhammed.youssef@gmail.com>
2023-06-25 06:31:21 +0300

    Add: movie service

---
+++ mvc/Interfaces/IMovieService.cs
@@ -0,0 +1,10 @@
+﻿using mvc.Data.Base;
+using mvc.Models;
+
+namespace mvc.Interfaces
+{
+    public interface IMovieService : IEntityBaseRepository<Movie>
+    {
+        Task<IEnumerable<Movie>> GetAllWithCinemaAsync();
+    }
+}
---
+++ mvc/Services/MovieService.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using mvc.Data;
+using mvc.Data.Base;
+using mvc.Interfaces;
+using mvc.Models;
+
+namespace mvc.Services
+{
+    public class MovieService : EntityBaseRepository<Movie>, IMovieService
+    {
+        private readonly AppDbContext _dbContext;
+
+        public MovieService(AppDbContext dbContext) : base(dbContext)
+        {
+            this._dbContext = dbContext;
+        }
+
+        public async Task<IEnumerable<Movie>> GetAllWithCinemaAsync()
+        {
+            return await _dbContext.Movie
+                .Include(m => m.Cinema)
+                .ToListAsync(); 
+        }
+    }
+}
