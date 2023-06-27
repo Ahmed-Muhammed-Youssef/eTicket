4d2840634252e6735c9a13d679da632b92186389
Ahmed Youssef <ahmed.muhammed.youssef@gmail.com>
2023-06-27 07:58:40 +0300

    Add: Order/OrderItem Configurations

---
+++ mvc/Data/Configurations/OrderConfigurations.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using mvc.Models;
+
+namespace mvc.Data.Configurations
+{
+    public class OrderConfigurations : IEntityTypeConfiguration<Order>
+    {
+        public void Configure(EntityTypeBuilder<Order> builder)
+        {
+            builder.HasKey(o => o.Id);
+            builder.Property(o => o.Email)
+                .IsRequired();
+
+            builder.HasMany(o => o.OrderItems)
+                .WithOne(oi => oi.Order)
+                .HasForeignKey(oi => oi.OrderId)
+                .IsRequired()
+                .OnDelete(DeleteBehavior.Cascade);
+
+        }
+    }
+}
---
+++ mvc/Data/Configurations/OrderItemConfigurations.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using mvc.Models;
+
+namespace mvc.Data.Configurations
+{
+    public class OrderItemConfigurations : IEntityTypeConfiguration<OrderItem>
+    {
+        public void Configure(EntityTypeBuilder<OrderItem> builder)
+        {
+            builder.HasKey(oi => oi.Id);
+
+            builder.Property(oi => oi.Price).IsRequired();
+            builder.Property(oi => oi.Amount).IsRequired();
+
+            // Relationships
+
+            builder.HasOne(oi => oi.Movie)
+                .WithMany()
+                .HasForeignKey(oi => oi.MovieId)
+                .IsRequired()
+                .OnDelete(DeleteBehavior.Cascade);
+
+        }
+    }
+}
