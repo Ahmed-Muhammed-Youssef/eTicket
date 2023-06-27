16962021f6d8137a4ddbd719af0aa04f07190009
Ahmed Youssef <ahmed.muhammed.youssef@gmail.com>
2023-06-27 07:58:19 +0300

    Add: Order/OrderItem Entities

---
+++ mvc/Models/Order.cs
@@ -0,0 +1,13 @@
+﻿namespace mvc.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public string Email { get; set; } = "";
+
+        // Foreign key
+        public int UserId { get; set; }
+        // Navigation Propertt
+        public List<OrderItem> OrderItems { get; set; } = new List<OrderItem> { };
+    }
+}
---
+++ mvc/Models/OrderItem.cs
@@ -0,0 +1,17 @@
+﻿namespace mvc.Models
+{
+    public class OrderItem
+    {
+        public int Id { get; set; }
+        public int Amount { get; set; }
+        public double Price { get; set; }
+
+        // Foreign Keys
+        public int MovieId { get; set; }
+        public int OrderId { get; set; }
+
+        // Navigation Properties
+        public Movie Movie { get; set; } = new Movie();
+        public Order Order { get; set; } = new Order();
+    }
+}
