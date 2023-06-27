1cc703e241f5f9088605750d5d9cf9a8e3ebf9ce
Ahmed Youssef <ahmed.muhammed.youssef@gmail.com>
2023-06-27 08:27:18 +0300

    Add: Cart/CartItem Configurations

---
+++ mvc/Data/Configurations/CartConfigurations.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using mvc.Models;
+
+namespace mvc.Data.Configurations
+{
+    public class CartConfigurations : IEntityTypeConfiguration<Cart>
+    {
+        public void Configure(EntityTypeBuilder<Cart> builder)
+        {
+            builder.HasKey(c => c.Id);
+            builder.Property(c => c.Email)
+                .IsRequired();
+
+            builder.HasMany(c => c.CartItems)
+                .WithOne(ci => ci.Cart)
+                .HasForeignKey(ci => ci.CartId)
+                .IsRequired()
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
---
+++ mvc/Data/Configurations/CartItemConfigurations.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using mvc.Models;
+
+namespace mvc.Data.Configurations
+{
+    public class CartItemConfigurations : IEntityTypeConfiguration<CartItem>
+    {
+        public void Configure(EntityTypeBuilder<CartItem> builder)
+        {
+            builder.HasKey(ci => ci.Id);
+
+            builder.Property(ci => ci.Price).IsRequired();
+            builder.Property(ci => ci.Amount).IsRequired();
+
+            // Relationships
+
+            builder.HasOne(ci => ci.Movie)
+                .WithMany()
+                .HasForeignKey(ci => ci.MovieId)
+                .IsRequired()
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
