e9a8929f011ddba89b10825e0356eda030f47564
Ahmed Youssef <ahmed.muhammed.youssef@gmail.com>
2023-06-27 08:26:57 +0300

    Add: Cart/CartItem entities

---
+++ mvc/Models/Cart.cs
@@ -0,0 +1,15 @@
+﻿namespace mvc.Models
+{
+    public class Cart
+    {
+        public int Id { get; set; }
+        public string Email { get; set; } = "";
+
+        // Foreign Keys
+        public int UserId { get; set; }
+        
+        // Navigation Properties
+        public List<CartItem> CartItems { get; set; } = new List<CartItem>();
+
+    }
+}
---
+++ mvc/Models/CartItem.cs
@@ -0,0 +1,17 @@
+﻿namespace mvc.Models
+{
+    public class CartItem
+    {
+        public int Id { get; set; }
+        public int Amount { get; set; }
+        public int Price { get; set; }
+
+        // Foreign Keys
+        public int CartId { get; set; }
+        public int MovieId { get; set; }
+
+        // Navigation Properties
+        public Movie Movie { get; set; } = new Movie();
+        public Cart Cart { get; set; } = new Cart();
+    }
+}
