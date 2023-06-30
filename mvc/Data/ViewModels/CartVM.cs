235c04ad25c98e43a7373ebe920c9bd650da34f6
Ahmed Youssef <ahmed.muhammed.youssef@gmail.com>
2023-06-30 19:40:42 +0300

    Add: cart view model

---
+++ mvc/Data/ViewModels/CartVM.cs
@@ -0,0 +1,10 @@
+﻿using mvc.Models;
+
+namespace mvc.Data.ViewModels
+{
+    public class CartVM
+    {
+        public required Cart Cart { get; set; }
+        public decimal Total { get; set; }
+    }
+}
