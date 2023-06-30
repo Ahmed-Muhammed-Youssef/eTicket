06411384331c18554339546ac59fb565bb4352f5
Ahmed Youssef <ahmed.muhammed.youssef@gmail.com>
2023-06-30 19:41:01 +0300

    Add: user data placeholder

---
+++ mvc/Models/UserPlaceHolder.cs
@@ -0,0 +1,8 @@
+﻿namespace mvc.Models
+{
+    public static class UserPlaceHolder
+    {
+        public static int UserId { get; } = 0;
+        public static string Email { get; } = "test@test";
+    }
+}
