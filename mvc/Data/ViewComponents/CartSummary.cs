14d38147b4f3c6ae8bf9be9ca975f7c66a65bd9a
Ahmed Youssef <ahmed.muhammed.youssef@gmail.com>
2023-07-01 12:34:28 +0300

    Add: ViewComponent for cart summary

---
+++ mvc/Data/ViewComponents/CartSummary.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using mvc.Interfaces;
+using mvc.Models;
+
+namespace mvc.Data.ViewComponents
+{
+    public class CartSummary : ViewComponent
+    {
+        private readonly IOrderService _orderService;
+        public CartSummary(IOrderService orderService)
+        {
+            _orderService = orderService;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var cart = await  _orderService
+                .GetUserCartAsync(UserPlaceHolder.UserId, UserPlaceHolder.Email);
+            if(cart == null)
+            {
+                return View(0);
+            }
+            return View(cart.CartItems.Count());
+        }
+    }
+}
