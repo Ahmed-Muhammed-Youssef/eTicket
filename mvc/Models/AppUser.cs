de46ba5179d23db8bce89b41e6e9768f1b9556c3
Ahmed Youssef <ahmed.muhammed.youssef@gmail.com>
2023-07-07 21:52:39 +0300

    Add: navigation properties to the user model
    Adds a list of the user's orders and cartitems

--- mvc/Models/AppUser.cs
+++ mvc/Models/AppUser.cs
@@ -6,5 +6,9 @@ namespace mvc.Models
     {
         public string FirstName { get; set; } = string.Empty;
         public string LastName { get; set; } = string.Empty;
+
+        // Navigation properties
+        public List<Order> Orders { get; set; } = new List<Order>();
+        public Cart? Cart { get; set; }
     }
 }
