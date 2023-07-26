4fd0c134fa1b13547a8da237ea63c0a4533a643a
Ahmed Youssef <ahmed.muhammed.youssef@gmail.com>
2023-07-26 20:49:19 +0300

    Add: responsive ui for login/movies/orders

---
+++ test/mvc.UnitTests/PayPalServiceTests.cs
@@ -0,0 +1,38 @@
+using mvc.Data.ViewModels;
+using mvc.Services;
+
+namespace mvc.UnitTests
+{
+    public class PayPalServiceTests
+    {
+        /*private readonly PayPalService paypalService = new PayPalService("AZDSTT6SfF4goNQUmXRjhUFwMJ2eoibtwNfV2bJetnrxrM46bn0VR9sNSS_988jx7lvRZgKgBrxfgrXg",
+                "EBviwmkeOQRs9obvMWuj46Rq0zrI8-mdF2u485g6aotejIZsmib9rr64CtkiQrvsP3pWLUs8yvZCw_Bj");
+        [Fact]
+        public async Task Authenticate_ShouldReturnToken_WhenAllParamatersAreValid()
+        {
+            // Arrange
+           
+
+            // Act 
+            var res = await paypalService.AuthenticateAsync();
+
+
+            // Assert
+            Assert.IsType<string>(res.access_token);
+        }
+
+        [Fact]
+        public async Task Order_ShouldReturnResponse_WhenAllParamatersAreValid()
+        {
+            // Arrange
+          
+
+            // Act 
+            var res = await paypalService.CreateOrder("200", "USD", "INV01");
+
+            await Console.Out.WriteLineAsync(res.status);
+            // Assert
+            Assert.Equal("200", res.status);
+        }*/
+    }
+}
