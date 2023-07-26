a789ad807dfcfce4ff89bab45b1b9429ecf4953a
Ahmed Youssef <ahmed.muhammed.youssef@gmail.com>
2023-07-26 20:17:48 +0300

    Update: order service to include the new field

--- src/mvc/Controllers/OrdersController.cs
+++ src/mvc/Controllers/OrdersController.cs
@@ -2,8 +2,10 @@
 using Microsoft.AspNetCore.Mvc;
 using mvc.Data.Static;
 using mvc.Data.ViewModels;
+using mvc.Helpers.Helpers;
 using mvc.Interfaces;
 using mvc.Models;
+using mvc.Services;
 using System.Security.Claims;
 
 namespace mvc.Controllers
@@ -11,13 +13,17 @@ namespace mvc.Controllers
     [Authorize]
     public class OrdersController : Controller
     {
+        private readonly PaypalClient _paypalClient;
         private readonly ICartService _cartService;
         private readonly IOrderService _orderService;
+        private readonly string clientId;
 
-        public OrdersController(ICartService cartService, IOrderService orderService)
+        public OrdersController(PaypalClient paypalClient, ICartService cartService, IOrderService orderService)
         {
+            _paypalClient = paypalClient;
             _cartService = cartService;
             _orderService = orderService;
+            clientId = paypalClient.ClientId;
         }
         public async Task<IActionResult> List()
         {
@@ -35,8 +41,6 @@ namespace mvc.Controllers
         [Authorize(Roles = UserRolesValues.Admin)]
         public async Task<IActionResult> ListAll()
         {
-            
-            
             var orders = await _orderService.
                 GetOrdersWithUsersAsync();
 
@@ -61,7 +65,7 @@ namespace mvc.Controllers
                 Cart = cart,
                 Total = cart.GetTotalPrice()
             };
-            return View(cartVM);
+            return View((cartVM, clientId));
         }
         public async Task<IActionResult> AddToCart(int id)
         {
@@ -90,7 +94,12 @@ namespace mvc.Controllers
             var cart = await _cartService.RemoveMovieFromCartAsync(id, userId, userEmail);
             return RedirectToAction(nameof(Index));
         }
-        public async Task<IActionResult> CompleteOrder()
+        public IActionResult Success()
+        {
+            return View("OrderCompleted");
+        }
+        [HttpPost]
+        public async Task<IActionResult> Order(CancellationToken cancellationToken)
         {
             var userId = User.FindFirst(ClaimTypes.NameIdentifier)!.Value;
             var userEmail = User.FindFirst(ClaimTypes.Email)!.Value;
@@ -99,12 +108,69 @@ namespace mvc.Controllers
                 return View("NotFound");
             }
             var cart = await _cartService.GetUserCartAsync(userId, userEmail);
-            if(cart == null)
+            if (cart == null)
             {
-                return View("NotFound");
+                cart = new Cart();
             }
-            var order = await _orderService.OrderAsync(cart);
-            return View("OrderCompleted");
+            try
+            {
+                // set the transaction price and currency
+                var price = cart.GetTotalPrice().ToString();
+                var currency = "USD";
+
+                // "reference" is the transaction key
+                var reference = cart.Id.ToString();
+
+                var response = await _paypalClient.CreateOrder(price, currency, reference);
+
+                return Ok(response);
+            }
+            catch (Exception e)
+            {
+                var error = new
+                {
+                    e.GetBaseException().Message
+                };
+
+                return BadRequest(error);
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Capture(string orderId, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var response = await _paypalClient.CaptureOrder(orderId);
+
+                // var reference = response?.purchase_units[0]?.reference_id;
+
+                // Put your logic to save the transaction here
+                // You can use the "reference" variable as a transaction key
+                var userId = User.FindFirst(ClaimTypes.NameIdentifier)!.Value;
+                var userEmail = User.FindFirst(ClaimTypes.Email)!.Value;
+                if (userId == null || userEmail == null)
+                {
+                    return View("NotFound");
+                }
+                var cart = await _cartService.GetUserCartAsync(userId, userEmail);
+                if (cart == null)
+                {
+                    cart = new Cart();
+                }
+                await _orderService.OrderAsync(cart, orderId);
+
+                return Ok(response);
+            }
+            catch (Exception e)
+            {
+                var error = new
+                {
+                    e.GetBaseException().Message
+                };
+
+                return BadRequest(error);
+            }
         }
     }
 }
--- src/mvc/Interfaces/IOrderService.cs
+++ src/mvc/Interfaces/IOrderService.cs
@@ -6,7 +6,7 @@ namespace mvc.Interfaces
     {
         
         Task<List<Order>> GetUserOrdersAsync(string userId, string email);
-        Task<Order> OrderAsync(Cart cart);
+        Task<Order> OrderAsync(Cart cart, string transactionKey);
         Task<List<Order>> GetOrdersWithUsersAsync();
 
     }
