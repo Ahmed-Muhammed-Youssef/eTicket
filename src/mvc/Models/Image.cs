b41c2404182ffa54770d1bfb055441905971b150
Ahmed Youssef <ahmed.muhammed.youssef@gmail.com>
2023-07-27 16:00:18 +0300

    Add: image model

---
+++ src/mvc/Models/Image.cs
@@ -0,0 +1,17 @@
+﻿using mvc.Data.Base;
+
+namespace mvc.Models
+{
+    public class Image : IEntityBase
+    {
+        public int Id { get; set; }
+        public string ImagePath { get; set; } = "";
+        public IFormFile? ImageFile { get; set; }
+
+
+        public static Image DefaultImageFactory()
+        {
+            return new Image();
+        }
+    }
+}
