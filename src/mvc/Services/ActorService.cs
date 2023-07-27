53b9e40d3ae2ace59b385190e8c969f1c11f6006
Ahmed Youssef <ahmed.muhammed.youssef@gmail.com>
2023-07-27 19:11:23 +0300

    Add: actor service to use server image upload

--- src/mvc/Services/ActorService.cs
+++ src/mvc/Services/ActorService.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.EntityFrameworkCore;
-using mvc.Data;
+﻿using mvc.Data;
 using mvc.Data.Base;
 using mvc.Interfaces;
 using mvc.Models;
@@ -8,8 +7,26 @@ namespace mvc.Services
 {
     public class ActorService : EntityBaseRepository<Actor>, IActorService
     {
-        public ActorService(AppDbContext dbContext) : base(dbContext)
+        private readonly AppDbContext _dbContext;
+        private readonly IImageUploadService _imageUploadService;
+
+        public ActorService(AppDbContext dbContext, IImageUploadService imageUploadService) : base(dbContext)
         {
+            _dbContext = dbContext;
+            _imageUploadService = imageUploadService;
+        }
+        public async Task<Actor> AddActorWithImageUplodaing(Actor actor)
+        {
+            var imagePath = await _imageUploadService.UploadAsync(actor.Image, nameof(Actor) + actor.FullName!);
+            
+            actor.Image.ImagePath = imagePath;
+            await _dbContext.Image.AddAsync(actor.Image);
+            await _dbContext.SaveChangesAsync();
+            
+            actor.ImageId = actor.Image.Id;
+            await _dbContext.Actor.AddAsync(actor);
+            await _dbContext.SaveChangesAsync();
+            return actor; 
         }
     }
 }
