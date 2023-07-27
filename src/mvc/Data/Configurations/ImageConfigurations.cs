c5647640aa67bc8db3d130494ffbdb5748d175c1
Ahmed Youssef <ahmed.muhammed.youssef@gmail.com>
2023-07-27 16:00:37 +0300

    Add: image model to dbcontext

---
+++ src/mvc/Data/Configurations/ImageConfigurations.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using mvc.Models;
+
+namespace mvc.Data.Configurations
+{
+    public class ImageConfigurations : IEntityTypeConfiguration<Image>
+    {
+        public void Configure(EntityTypeBuilder<Image> builder)
+        {
+            // porperties
+
+            builder.HasKey(i => i.Id);
+
+           builder.Property(i => i.ImagePath)
+                .IsRequired();
+
+            builder.Ignore(i => i.ImageFile);
+
+            // relationships
+
+            // actors
+            builder.HasOne<Actor>()
+                .WithOne(a => a.Image)
+                .HasForeignKey<Actor>(a => a.ImageId)
+                .IsRequired(false)
+                .OnDelete(DeleteBehavior.SetNull);
+
+            // cinemas
+            builder.HasOne<Cinema>()
+                .WithOne(c => c.Image)
+                .HasForeignKey<Cinema>(c => c.ImageId)
+                .IsRequired(false)
+                .OnDelete(DeleteBehavior.SetNull);
+
+            // producers
+            builder.HasOne<Producer>()
+               .WithOne(p => p.Image)
+               .HasForeignKey<Producer>(p => p.ImageId)
+               .IsRequired(false)
+               .OnDelete(DeleteBehavior.SetNull);
+
+            // movies
+            builder.HasOne<Movie>()
+               .WithOne(m => m.Image)
+               .HasForeignKey<Movie>(m => m.ImageId)
+               .IsRequired(false)
+               .OnDelete(DeleteBehavior.SetNull);
+        }
+    }
+}
