cd5b32547f8cbc5a7c21a37f2edd1cdc5260f5f1
Ahmed Youssef <ahmed.muhammed.youssef@gmail.com>
2023-07-30 16:16:30 +0300

    Add: cinema view model

---
+++ src/mvc/Data/ViewModels/CinemaVM.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace mvc.Data.ViewModels
+{
+    public class CinemaVM
+    {
+        [Required]
+        [DisplayName("Name")]
+        public string Name { get; set; } = "";
+
+        [Required]
+        [DisplayName("Description")]
+        public string Description { get; set; } = "";
+        public IFormFile? ImageFile { get; set; }
+    }
+}
