34abe1b2d0bd9f77b1785181fd170f7d549a5ac5
Ahmed Youssef <ahmed.muhammed.youssef@gmail.com>
2023-07-30 13:16:45 +0300

    Add: view model for producer entity

---
+++ src/mvc/Data/ViewModels/ProducerVM.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace mvc.Data.ViewModels
+{
+    public class ProducerVM
+    {
+        [Required]
+        [DisplayName("Full Name")]
+        public string FullName { get; set; } = "";
+
+        [Required]
+        [DisplayName("Bio")]
+        public string Bio { get; set; } = "";
+        public IFormFile? ImageFile { get; set; }
+    }
+}
