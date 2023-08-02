0910db4371d6e4948e0d4b548051c80ea8a54448
Ahmed Youssef <ahmed.muhammed.youssef@gmail.com>
2023-08-02 20:17:33 +0300

    Rework: UI

--- src/mvc/Data/ViewComponents/CartSummary.cs
+++ src/mvc/Data/ViewComponents/CartSummary.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using mvc.Interfaces;
 using System.Security.Claims;
 
@@ -11,6 +12,7 @@ namespace mvc.Data.ViewComponents
         {
             _cartService = cartService;
         }
+        [Authorize]
         public async Task<IViewComponentResult> InvokeAsync()
         {
             var userId = UserClaimsPrincipal.FindFirst(ClaimTypes.NameIdentifier)!.Value;
