9d1045fd0aed3c9cb156276e59b9adfa453a00db
Ahmed Youssef <ahmed.muhammed.youssef@gmail.com>
2023-08-03 20:27:45 +0300

    Add: user controller
    adds the index method

---
+++ src/mvc/Controllers/UsersController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using mvc.Data.Static;
+using mvc.Data.ViewModels;
+using mvc.Models;
+
+namespace mvc.Controllers
+{
+    [Authorize(Roles = UserRolesValues.Admin)]
+    public class UsersController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public UsersController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var users = await _userManager.Users.Select(u => new UserVM() 
+            { 
+                Id = u.Id,
+                Email = u.Email ?? "-",
+                FullName = u.GetFullName(),
+                UserName = u.UserName ?? "-",
+                PhoneNumber = u.PhoneNumber ?? "-"
+            
+            }).ToListAsync();
+            if (users is null)
+            {
+                users = new List<UserVM>();
+            }
+            return View(users);
+        }
+    }
+}
