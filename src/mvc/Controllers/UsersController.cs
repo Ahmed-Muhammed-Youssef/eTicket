3c061688dcf534f9c3aead1f3fbb2ab645a7ad7c
Ahmed Youssef <ahmed.muhammed.youssef@gmail.com>
2023-08-03 20:51:22 +0300

    Add: user details view

--- src/mvc/Controllers/UsersController.cs
+++ src/mvc/Controllers/UsersController.cs
@@ -5,6 +5,7 @@ using Microsoft.EntityFrameworkCore;
 using mvc.Data.Static;
 using mvc.Data.ViewModels;
 using mvc.Models;
+using System.Linq;
 
 namespace mvc.Controllers
 {
@@ -33,6 +34,29 @@ namespace mvc.Controllers
                 users = new List<UserVM>();
             }
             return View(users);
+        }
+        public async Task<IActionResult> Details(string id)
+        {
+            var u = await _userManager.Users
+                .Include(u => u.Orders)
+                .Include(u => u.Cart)
+                .FirstOrDefaultAsync(u => u.Id == id);
+            if (u is null)
+            {
+                return View("NotFound");
+            }
+            var userVM = new DetailedUserVM()
+            {
+                Id = u.Id,
+                Email = u.Email ?? "-",
+                FullName = u.GetFullName(),
+                UserName = u.UserName ?? "-",
+                PhoneNumber = u.PhoneNumber ?? "-",
+                Orders = u.Orders,
+                Cart = u.Cart,
+                MoneyPaied = u.Orders.Aggregate(0M, (acc, cur) => acc + cur.GetTotalPrice(), acc => acc)
+            };
+            return View(userVM);
         }
     }
 }
---
+++ src/mvc/Data/ViewModels/DetailedUserVM.cs
@@ -0,0 +1,11 @@
+﻿using mvc.Models;
+
+namespace mvc.Data.ViewModels
+{
+    public class DetailedUserVM : UserVM
+    {
+        public List<Order> Orders { get; set; } = new List<Order>();
+        public Cart? Cart { get; set; }
+        public decimal MoneyPaied { get; set; }
+    }
+}
