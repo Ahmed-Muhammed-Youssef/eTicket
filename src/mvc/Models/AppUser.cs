7608c43599e3b08503df26a5d152cf87e82ba1a1
Ahmed Youssef <ahmed.muhammed.youssef@gmail.com>
2023-08-04 05:18:53 +0300

    Add: user address model

--- src/mvc/Models/AppUser.cs
+++ src/mvc/Models/AppUser.cs
@@ -7,7 +7,11 @@ namespace mvc.Models
         public string FirstName { get; set; } = string.Empty;
         public string LastName { get; set; } = string.Empty;
 
+        // Foreign Keys
+        public int UserAddressId { get; set; }
+
         // Navigation properties
+        public UserAddress UserAddress { get; set; } = new UserAddress();
         public List<Order> Orders { get; set; } = new List<Order>();
         public Cart? Cart { get; set; }
         public string GetFullName() => FirstName + (string.IsNullOrEmpty(LastName) ? "" : " " + LastName);
